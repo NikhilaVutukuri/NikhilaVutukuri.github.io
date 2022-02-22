5219c84c8fd32ae0ff8cd394c4ab3bebe61b7672
Nikhila Vutukuri <37280400+NikhilaVutukuri@users.noreply.github.com>
2022-02-21 17:19:54 -0800

    Update app.js

--- LABeauty2.O/app.js
+++ LABeauty2.O/app.js
@@ -4,6 +4,9 @@ fetch('data.json')
     let section2 = data.section2;
     let section3 = data.section3;
     let section4 = data.section4;
+    document.getElementById("cards").children[0].children[0].src="./images/"+section2[0].image;
+    document.getElementById("cards").children[0].children[1].innerHTML=section2.heading;
+    document.getElementById("cards").children[0].children[2].innerHTML=section2.text;
 });
 
 setTimeout(function(){
